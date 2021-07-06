8e6f19e5bb2530fdf2fba01b10414a695f054244
Nyera <34568106+nyera@users.noreply.github.com>
2021-07-06 14:33:25 +0200

    Update jquery.js

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
             if (width >= 400) {
 
                 if (scroll_pos > 10 && canSee) {
+                    /*
                     $("#logo").animate({ left: "118%" }, 1500);
                     $("#intro-background").animate({ width: "110%" }, 1400);
                     $(".bio-1").delay(000).fadeIn(1500);
@@ -23,6 +24,19 @@ $(document).ready(function () {
 
                     $("#logo").animate({ left: "119%" }, 3000, function () {
                         $("#logo").animate({ left: "120%" }, 50);
+                     */   
+                     // the New code just remove the comment to work 
+                    $("#logo").animate({ right: "-90%" },1600);
+                    $("#intro-background").animate({ width: "110%" }, 1600);
+                    $(".bio-1").delay(000).fadeIn(1500);
+                    $(".bio-2").delay(250).fadeIn(1500);
+                    $(".bio-3").delay(750).fadeIn(1500);
+                    $(".bio-4").delay(1000).fadeIn(1500);
+                    
+
+                    $("#logo").animate({ right: "-90%" }, 1400, function () {
+                        $("#logo").animate({ right: "-90%" }, 100);
+                        
                     });
 
                     canSee = false;
@@ -31,6 +45,7 @@ $(document).ready(function () {
                 else if (scroll_pos <= 10 && !canSee) {
 
                     $("#logo").animate({ left: "119%" }, 50, function () {
+                        /*
                         $(".bio-1").hide(350);
                         $(".bio-2").hide(350);
                         $(".bio-3").hide(350);
@@ -41,7 +56,19 @@ $(document).ready(function () {
                         $("#logo").animate({ left: "118%" }, 50, function () {
                             $("#logo").animate({ left: "50%" }, 1600);
                             $("#intro-background").animate({ width: "50%" }, 1400);
+*/
+                        //the new code 
+                         $("#logo").animate({ right: "-90%" },50, function () {
+                        $(".bio-1").fadeOut(350);
+                        $(".bio-2").fadeOut(350);
+                        $(".bio-3").fadeOut(350);
+                        $(".bio-4").fadeOut(350);
 
+                // To be fair, can definitely improve on the hide motion
+
+                        $("#logo").animate({ right: "90%" }, 1450, function () {
+                            $("#logo").animate({ right: "90%" }, 1400);
+                            $("#intro-background").animate({ width: "50%" }, 1450);
                         });
                     });
                     canSee = true;
